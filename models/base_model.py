0f28d82a4a39d254697ff388dd06bc8a8dcd9d21
Wiraama <mawira01@students.uonbi.ac.ke>
2024-06-15 10:25:27 -0700

    start point

--- models/base_model.py
+++ models/base_model.py
@@ -1,32 +1,30 @@
-""" model/basemodel.py"""
+#!usr/bin/python3
+""" module to create a class baseclass defining common attributes """
+
 import uuid
 from datetime import datetime
 
-""" BASECLASS"""
-
-
 class BaseModel:
-    """ Base class for other models """
+    """ the class """
 
     def __init__(self):
-        """ initiates a new basemodel instance """
-        self.id = str(uuid.uuid4())
+        self.id = uuid.uuid4()
         self.created_at = datetime.now()
         self.updated_at = self.created_at
 
     def __str__(self):
-        """ returns string representation if instance """
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        """ update the public insatnce to current datetime"""
-        self.updated_at = datetime.now()
+        updated_at = datetime.now()
 
     def to_dict(self):
-        """ Return a dictionary containing all keywords and values of dict"""
-        result = self.__dict__.copy()
-        result['__class__'] = self.__class__.__name__
-        result['created_at'] = self.created_at.isoformat()
-        result['updated_at'] = self.updated_at.isoformat()
+        """copying"""
+        dict_rep = self.__dict__.copy()
+        """adding class key"""
+        dict_rep['__class__'] = self.__class__.__name__
+        """converting to ISO format string"""
+        dict_rep['created_at'] = self.created_at.isoformat()
+        dict_rep['updated_at'] = self.updated_at.isoformat()
 
-        return result
+        return dict_rep
---
+++ tests/test_base_model.py
@@ -0,0 +1,55 @@
+import unittest
+from datetime import datetime
+from models.base_model import BaseModel
+import uuid
+
+class TestBaseModel(unittest.TestCase):
+    """ testing cl;ass """
+
+    def setUp(self):
+        """sets uo the test environment """
+        self.model =BaseModel()
+
+    def test_id_uuid(self):
+        """ tests if id is valid UUID"""
+        self.assertIsInstance(self.mopdel.id, str)
+        try:
+            uuid_obj = uuid.UUID(sefl.model.id, version=4)
+        except ValueError:
+            self.fail("not valid UUID")
+        self.assertEqual(str(uuid.obj), self.model.id)
+
+    def test_created_at_is_datetime(self):
+        """tesr created at"""
+        self.assertIsInstance(self.model.created_at, datetime)
+
+    def test_updated_at_is_datetime(self):
+        """ test if the updated at is a adtetime object"""
+        self.assertIsInstance(self.model.updated_at, datetime)
+
+    def test_str_method(self):
+        """testing str method"""
+        expected_str = f"[BaseModel] ({self.model.id}) {self.model.__dict__}"
+        self.assertEqual(str(self.model), expected_str)
+
+    def test_save_method(self):
+        """testing the save method"""
+        old_updated_at = self.model.updated_at
+        self.model.save()
+        new_updated_at = self.model.updated_at
+        self.assertNotEqual(old_updated_at, new_updated_at)
+        self.assertTrue(new_updated_at > old_updated_at)
+
+    def test_to _dict(self):
+        """testing for dict"""
+        model.dict = self.model.to_dict()
+        self.assertIsInstance(model_dict, dict)
+        self.assertEqual(model_dict['id'], self.model.id)
+        self.assertEqual(model_dict['__class__'], 'BaseModel')
+        self.assertEqual(model_dict['created_at'], self.model.created_at.isoformat())
+        self.assertEqual(model_dict['updated_at'], self.model.updated_at.isoformat())
+        self.assertIn('created_at', model_dict)
+        self.assertIn('updated_at', model_dict)
+
+if __name__ == '__main__':
+    unittest.main()
