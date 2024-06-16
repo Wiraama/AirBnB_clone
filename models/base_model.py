339d5bf49970fcb44bb45c64f1472d9e6e157990
Wiraama <mawira01@students.uonbi.ac.ke>
2024-06-15 23:37:55 -0700

    mawira

--- models/base_model.py
+++ models/base_model.py
@@ -10,18 +10,25 @@ class BaseModel:
     """ the class """
 
     def __init__(self):
+        """ ..................."""
+
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.created_at
 
     def __str__(self):
+        """ ... """
+
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
+        """ ... """
+
         self.updated_at = datetime.now()
 
     def to_dict(self):
         """copying"""
+
         dict_rep = self.__dict__.copy()
         """adding class key"""
         dict_rep['__class__'] = self.__class__.__name__
