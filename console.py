cc0625567173638f31641d1f2ad8ce3c8ba65a9b
Wiraama <mawira01@students.uonbi.ac.ke>
2024-06-16 16:45:19 -0700

    project

--- console.py
+++ console.py
@@ -4,21 +4,24 @@ module to interpreate for an application
 """
 import cmd
 
+
 class HBNBCommand(cmd.Cmd):
+    """ class """
     prompt = "(hbnb) "
 
     def do_quit(self, arg):
         """Quit command to exit the program"""
         return True
 
-    def do_EoF(self, arg):
-        """EoF command to exit thr program"""
+    def do_EOF(self, arg):
+        """EOF command to exit thr program"""
         print()
         return True
 
     def emptyline(self):
         """Do nothing empty input"""
         pass
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
