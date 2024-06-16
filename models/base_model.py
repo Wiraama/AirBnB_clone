096d2ba78bd991fa8a7f4e7ad3abde71767f77e4
Wiraama <mawira01@students.uonbi.ac.ke>
2024-06-16 00:11:41 -0700

    project

--- models/base_model.py
+++ models/base_model.py
@@ -12,13 +12,13 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """ ..................."""
 
-        if kwargs != None:
+        if kwargs:
             for key, value in kwargs.items():
                 if key == 'created_at' or key == 'updated_at':
                     setattr(self, key, datetime.strtime(
                         value, '%Y-%m-%dT%H:%M:%S.%f'))
                 elif key != '__class__':
-                    setattr(self, key,value)
+                    setattr(self, key, value)
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.created_at
