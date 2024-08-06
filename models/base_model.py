6c51bf45c4c949d45e6e6730d17268f75373d654
mawira <mawira@students.uonbi.ac.ke>
2024-08-06 08:25:16 +0300

    basemodule

--- models/base_model.py
+++ models/base_model.py
@@ -1,48 +1,30 @@
-#!usr/bin/python3
-""" module to create a class baseclass defining common attributes """
-
+#!/usr/bin/python3
 import uuid
 from datetime import datetime
 
 
 class BaseModel:
-    """ the class """
+    """ this class should define all attributes/methods for other classes """
 
-    def __init__(self, *args, **kwargs):
-        """ ..................."""
-
-        if kwargs:
-            for key, value in kwargs.items():
-                if key == 'created_at' or key == 'updated_at':
-                    setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
-                elif key != '__class__':
-                    setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = self.created_at
-
-    def __str__(self):
-        """ ... """
-
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def save(self):
-        """ ... """
-
         self.updated_at = datetime.now()
-        from models import storage
-        storage.new(self)
-        storage.save()
+    
 
     def to_dict(self):
-        """copying"""
-
+        """ dictionary from instances """
         dict_rep = self.__dict__.copy()
-        """adding class key"""
-        dict_rep['__class__'] = self.__class__.__name__
-        """converting to ISO format string"""
+
         dict_rep['created_at'] = self.created_at.isoformat()
         dict_rep['updated_at'] = self.updated_at.isoformat()
+        dict_rep['__class__'] = self.__class__.__name__
 
         return dict_rep
+
+
+    def __str__(self):
+        return("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
--- tests/__init__.py
+++
@@ -1,9 +0,0 @@
-from models.base_model import BaseModel
-from models.engine.file_storage import FileStorage
-
-__all__ = ["BaseModel"]
-
-storage = FileStorage()
-storage.reload()
-
-BaseModel.sorage = storage
