ccafda612fb660bb84c0e0fc23c90d521d7f17da
Wiraama <mawira01@students.uonbi.ac.ke>
2024-09-20 12:36:38 +0300

    Airbnb

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
+import sys
+import os
 import unittest
 from datetime import datetime
-from base_model import BaseModel
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from models.base_model import BaseModel
+
 
 class TestBaseModel(unittest.TestCase):
     """ test for each function """
