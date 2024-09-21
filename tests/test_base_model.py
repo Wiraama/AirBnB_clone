8278cdf5d5023c90fadd537c3de57ee4176f3bd6
Wiraama <mawira01@students.uonbi.ac.ke>
2024-09-21 10:08:57 +0300

    AirBnB

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import unittest
 from datetime import datetime
-from models.base_model import BaseModel
+from models.tmp_base_model import BaseModel
 
 class TestBaseModel(unittest.TestCase):
     
